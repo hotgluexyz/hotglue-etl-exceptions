de8bb8f4c13de4cdd27034b7ebe4c93cf48c6265
LiorB-D <liorbd@outlook.com>
2025-12-29 18:15:04 -0500

    Initial setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""Setup configuration for hotglue-etl-exceptions package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hotglue-etl-exceptions',
+    version='0.1.0',
+    description='Exception classes for Hotglue ETL',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
+
